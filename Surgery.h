5661eb1a2de217c097b46c6237e395e152c75e05
Dre <31423756+jinxTelesis@users.noreply.github.com>
2018-02-24 16:25:34 -0500

    Header
    First class by john had switch to select charge
    Second had get//setters, combined his two
    cout, cin display functionality moved out to main

--- Surgery.h
+++ Surgery.h
@@ -5,11 +5,34 @@ using namespace std;
 class Surgery
 {
 private:
-	float surgerycost; //getters
-	string surgerytype; //getters
+	int mCharge =0; 
+	//string mSurgeryType;
+	//string mAmputation; // 1st surgery
+	//string mRecapitation; // 2nd surgery
+	//string mSexualReassignement; // 3rd surgery
+	//string mBiopsy; // 4th surgery
+	//string mLivertr; // 5th surgery
+	int mAmpCost;
+	int mRecapCost;
+	int mSrsCost;
+	int mBiopCost;
+	int mLiverTrCost;
 public:
-	Surgery(float, string); // setters
-	float getcost(); //setters
-	string getsurgerytype(); // setters
-	void getsurgerydisplay(float& totalcost1); // setters
+	Surgery(); // added
+	Surgery(int Amp, int Recap, int Srs, int Bio, int LiverTr);
+	int getAmp();
+	void setAmp(int amp);
+	int getRecap();
+	void setRecap(int recap);
+	int getSrs();
+	void setSrs(int recap);
+	int getBiospy();
+	void setBiospy(int bio);
+	int getLiverTr();
+	void setLiverTr(int liv);
+
+	int picCharge(char charge);
+	//float getcost(); 
+	//string getsurgerytype(); 
+	//void getSurgerydisplay(float& totalcost1); 
 };
