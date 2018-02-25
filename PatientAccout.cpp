e17a1e70168ea75e38b0b3416238f6ce19323827
Dre <31423756+jinxTelesis@users.noreply.github.com>
2018-02-24 20:46:39 -0500

    Update PatientAccout.cpp
    updated so charges uses other classes via composition

--- PatientAccout.cpp
+++ PatientAccout.cpp
@@ -1,26 +1,54 @@
-#include "PatientAccount.h"
+#pragma once
 #include <iostream>
+#include "PatientAccout.h"
+#include "Pharmacy.h"
+#include "Surgery.h"
+#include <string>
+
 using namespace std;
 
 PatientAccount::PatientAccount()
 {
-	name = ""; 
+	name = "";
 	idNumber = 0;
 	charges = 0;
-	numberDays = 0;
-	dailyRate = 0;
+	numberDays = 1;
+	dailyRate = 1;
 }
 
-PatientAccount::PatientAccount(string name, int idNumber, double dailyRate) {
+PatientAccount::PatientAccount(string name, int idNumber, int dailyRate) {
 	this->name = name;
 	this->idNumber = idNumber;
 	this->charges = 0;
-	this->numberDays = 0;
-	this->dailyRate = dailyRate; 
+	this->numberDays = 1;
+	this->dailyRate = dailyRate;
+}
+/*
+PatientAccount(Pharmacy &phar, Surgery &surg, string name, int idNumber, int dailyrate)
+{
+	this->idNumber = idNumber;
+	this->dailyrate = dailyrate;
+	this->name = name;
+}
+*/
+/*
+PatientAccount::PatientAccount(Surgery &surg, Pharmacy &phar, string name, int idNumber, int dailyrate)
+{
+	// think on this
+}
+*/
+
+int PatientAccount::calChargesForStay()
+{
+	charges = surg.getCharge() + phar.getCharge() + (dailyRate * numberDays);
+	return charges;
 }
 
 void PatientAccount::setDailyRate(int dailyRate) {
-	this->dailyRate = dailyRate;
+	if (dailyRate > 0)
+		this->dailyRate = dailyRate;
+	else
+		this->dailyRate = 1;
 }
 
 int PatientAccount::getDailyRate() {
@@ -28,7 +56,10 @@ int PatientAccount::getDailyRate() {
 }
 
 void PatientAccount::setDaysInHospital(int numberDays) {
-	this->numberDays = numberDays;
+	if (numberDays > -1)
+		this->numberDays = numberDays;
+	else
+		numberDays = 0;
 }
 
 int PatientAccount::getDaysInHospital() {
@@ -36,7 +67,9 @@ int PatientAccount::getDaysInHospital() {
 }
 
 void PatientAccount::setTotalCharges(int charges) {
+	cout << "override charges for special cases" << endl;
 	this->charges = charges;
+	cout << "overidden charge is " << this->charges << endl;
 }
 
 int PatientAccount::getTotalCharges() {
@@ -44,7 +77,10 @@ int PatientAccount::getTotalCharges() {
 }
 
 void PatientAccount::setIdNumber(int idNumber) {
-	this->idNumber = idNumber;
+	if (idNumber >= 1)
+		this->idNumber = idNumber;
+	else
+		this->idNumber = 1;
 }
 
 int PatientAccount::getIdNumber() {
